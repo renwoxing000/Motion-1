5c4f014000d54697d8af334ddb772f492a5d2f2b
HL Ren <honglinren000@gmail.com>
2013-09-24 23:41:05 +0800

    Signed-off-by: HL Ren <honglinren000@gmail.com>

--- src/Motion/main.cpp
+++ src/Motion/main.cpp
@@ -1,19 +1,16 @@
 // Main entry for Motion Application
-//#include "MotionApp.h"
-#include "SampleBrowser.h"
+#include "MotionApp.h"
 
-//INT WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR cmdLine, INT) {
-int main(){
+INT WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR cmdLine, INT) {
 	/* 
 	    argv[0] [ignored] the command to run this application
 	    argv[1] startSampleIndex passed to MotionApp()
 	 */
- //   int argc = __argc;
- //   char** argv = __argv;
-	//int startSampleIndex = (argc >= 2) ? startSampleIndex = Ogre::StringConverter::parseInt(Ogre::String(argv[2]), -1) : -1;
+    int argc = __argc;
+    char** argv = __argv;
+	int startSampleIndex = (argc >= 2) ? startSampleIndex = Ogre::StringConverter::parseInt(Ogre::String(argv[2]), -1) : -1;
 
-	//MotionApp app(startSampleIndex);
-	OgreBites::SampleBrowser app;
+	OgreBites::SampleBrowser app(false, startSampleIndex);
 	app.go();
 	return 0;
 }
